9b688db29e9d2cb3667b4835550b570739077d58
chris <104324755+cjurgens17@users.noreply.github.com>
2023-07-27 17:30:53 -0400

    finishing up project page

--- assets/js/projectone.js
+++ assets/js/projectone.js
@@ -1,10 +1,11 @@
-const bitContainer = document.querySelector('.bit-container');
-const bit = document.querySelector('.bits');
 
-function getBitTranslation() {
-    const bitContainerWidth = bitContainer.clientWidth;
-    const bitWidth = bit.clientWidth;
-    return `${bitContainerWidth - bitWidth}px`;
-}
 
-document.documentElement.style.setProperty('--bit-distance', getBitTranslation());
+
+
+
+
+
+
+
+
+
